3360a1c728dc2182241644a692cb89a62a12d4dc
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-01-19 17:37:06 +0400

    feat:added colors to data json

---
+++ src/menuData.js
@@ -0,0 +1,12 @@
+const menuItems = [
+  { color: "#DEF4FC", name: "MERCURY" },
+  { color: "#F7CC7F", name: "VENUS" },
+  { color: "#545BFE", name: "EARTH" },
+  { color: "#FF6A45", name: "MARS" },
+  { color: "#ECAD7A", name: "JUPITER" },
+  { color: "#FCCB6B", name: "SATURN" },
+  { color: "#65F0D5", name: "URANUS" },
+  { color: "#497EFA", name: "NEPTUNE" },
+];
+
+export default menuItems;
